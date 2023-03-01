f8e2f4bf942b2757810660e06257174bb46d3fcc
PC1 <xlobach@stuba.sk>
2023-03-01 01:29:06 +0100

    Initial commit

---
+++ FEI-UIM-OOP-LS-21-cvicenie1/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.feiuimoopls21cvicenie1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.feiuimoopls21cvicenie1 to javafx.fxml;
+    exports com.example.feiuimoopls21cvicenie1;
+}
